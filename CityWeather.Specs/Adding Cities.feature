abb3ba4b5bf05e39321792e82a2b350b9327b54a
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2019-03-02 23:27:01 +0000

    Added some basic placeholder specflow BDD tests for creating cities.

---
+++ CityWeather.Specs/Adding Cities.feature
@@ -0,0 +1,27 @@
+﻿Feature: Adding Cities
+	As an Api consumer
+	I want to be able to add new cities
+	So that the system can be continually extended to serve new clients
+
+Scenario: User attempts to add the first new city
+	Given That no example cities exist in the system
+	But That the city "Oxford" does not exist in the system
+	When the system is instructed to add the city "Oxford"
+	Then the city "Oxford" should be present in the system
+	And the total number of cities should equal 1.
+
+Scenario: User attempts to add additional cities
+	Given That example cities already exist in the system
+	But  the city "Oxford" does not exist in the system
+	When the system is instructed to add the city "Oxford"
+	Then the city "Oxford" should be present in the system
+	And the total number of cities should equal 3.
+
+Scenario: User attempts to add a city that already exists
+	Given That example cities already exist in the system
+	When the system is instructed to add the city "London"
+	Then the system should raise an error saying "The current city already exists".
+
+# Scenario outlines. I would have liked to demonstrate my knowledge of scenario outlines here but 
+# I didn't think any of these tests merited their use. In the interests of simplicity I ommited them
+# As they would be overkill here.
---
+++ CityWeather.Specs/AddingCitiesSteps.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using System.Linq;
+using FluentAssertions;
+
+namespace CityWeather.Specs
+{
+    [Binding]
+    public class AddingCitiesSteps
+    {
+        private List<City> _systemCities;
+
+        [Given(@"That no example cities exist in the system")]
+        public void GivenThatNoExampleCitiesExistInTheSystem()
+        {
+            _systemCities = new List<City>();
+        }
+
+        /* I've put two attributes here just to make the feature file language cleaner */
+        [Given(@"That the city ""(.*)"" does not exist in the system")]
+        [Given(@"the city ""(.*)"" does not exist in the system")]
+        public void GivenThatTheCityDoesNotExistInTheSystem_(string cityName)
+        {
+            _systemCities.RemoveAll(x=>x.Name == cityName);
+        }
+        
+        [Given(@"That example cities already exist in the system")]
+        public void GivenThatExampleCitiesAlreadyExistInTheSystem()
+        {
+            _systemCities = new List<City>()
+            {
+                new City() { Name = "London" },
+                new City() { Name = "Oxford" },
+                new City() { Name = "Sheffield" }
+            };
+        }
+        
+        [When(@"the system is instructed to add the city ""(.*)""")]
+        public void WhenTheSystemIsInstructedToAddTheCity(string cityName)
+        {
+            _systemCities.Add(new City() { Name = cityName });
+        }
+
+        [Then(@"the city ""(.*)"" should be present in the system")]
+        public void ThenTheCityShouldBePresentInTheSystem(string cityName)
+        {
+            _systemCities
+                .FirstOrDefault(x => x.Name == cityName)
+                .Should().NotBeNull();
+        }
+
+        [Then(@"the total number of cities should equal (.*)\.")]
+        public void ThenTheTotalNumberOfCitiesShouldEqual_(int expectedCityCount)
+        {
+            _systemCities.Count().Should().Be(expectedCityCount);
+
+        }
+
+        [Then(@"the system should raise an error saying ""(.*)""\.")]
+        public void ThenTheSystemShouldRaiseAnErrorSaying_(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+
+    internal class City
+    {
+        public string Name { get; set; }
+    }
+}
