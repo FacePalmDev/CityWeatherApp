e00e2fb9d96ff6fd10b45a217e716c59e20486a4
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2019-03-03 14:36:09 +0000

    Added a stub repository just to test internal access

---
+++ CityWeather.Data/Repository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CityWeather.Data.Contracts;
+
+namespace CityWeather.Data
+{
+    public class Repository<T> : IRepository<T>
+    {
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
