96a9528ecdcca485c52fbd725aeb9cf169cd3c57
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2019-03-03 17:26:36 +0000

    Repository creates entities

--- CityWeather.Data/Repository.cs
+++ CityWeather.Data/Repository.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CityWeather.Data.Contracts;
+using CityWeather.Data.Entities;
 
 namespace CityWeather.Data
 {
-    public class Repository<T> : IRepository<T>
+    public class Repository<C, T> : IRepository<T> 
+        where C: DbContext 
+        where T: class
     {
+        private readonly C _context;
+        private DbSet<T> _dbSet;
+
+        public Repository(C context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        
         public void Create(T item)
         {
-            throw new NotImplementedException();
+            _dbSet.Add(item);
         }
 
         public IEnumerable<T> Read()
