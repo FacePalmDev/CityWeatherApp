ccbaa6c7edb6777cbe0f1c61d0fff3a31ba2ac67
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2019-03-03 17:22:08 +0000

    Gave the mocking proxy access to our internal data contracts

--- CityWeather.Data.Contracts/InternalsVisibleTo.cs
+++ CityWeather.Data.Contracts/InternalsVisibleTo.cs
@@ -3,6 +3,7 @@
 [assembly: InternalsVisibleTo("CityWeather.Data")]
 [assembly: InternalsVisibleTo("CityWeather.Data.Tests")]
 [assembly: InternalsVisibleTo("CityWeather.Specs")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 namespace CityWeather.Data.Contracts
 {
     
