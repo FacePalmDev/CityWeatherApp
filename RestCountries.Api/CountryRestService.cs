191640729a802e50236592cf1c04020ed5f0d619
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2019-03-05 14:44:33 +0000

    refactor: abstracted base RESTful functionality for WeatherService

--- RestCountries.Api/CountryRestService.cs
+++ RestCountries.Api/CountryRestService.cs
@@ -1,22 +1,42 @@
 ﻿using CityWeather.Common.Settings;
 using RestCountries.Models;
+using RestServices;
 using RestSharp;
 using RestSharp.Serialization.Json;
 
 namespace RestCountries.Api
 {
-    public class CountryRestService : ICountryRestService
+    /// <summary>
+    /// The Country Rest Service Helper.
+    /// </summary>
+    /// <seealso cref="RestServices.RestService{RestCountries.Models.Country}" />
+    /// <seealso cref="RestCountries.Api.ICountryRestService" />
+    public class CountryRestService : RestService<Country>, ICountryRestService
     {
+        /// <summary>
+        /// Gets the rest client.
+        /// </summary>
+        public override RestClient Client { get; }
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CountryRestService"/> class.
+        /// </summary>
+        public CountryRestService()
+        {
+            Client = new RestClient(UrlSettings.CountryApiBaseUrl);
+        }
+
+        /// <summary>
+        /// Gets the country data.
+        /// </summary>
+        /// <param name="countryCode">The country code.</param>
+        /// <returns></returns>
         public Country GetCountryData(string countryCode)
         {
-            var client = new RestClient(UrlSettings.CountryApiBaseUrl);
             var request = new RestRequest($"alpha/{countryCode}", Method.GET);
 
-            var response = client.Execute(request);
-
-            var deserializer = new JsonDeserializer();
-
-            return deserializer.Deserialize<Country>(response);
+            return GetDeserializedObject(request);
         }
+
     }
 }
---
+++ RestServices/RestService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Serialization.Json;
+
+namespace RestServices
+{
+    // sure this could go in common but I thought it's only used by the domain layer right now.
+    public abstract class RestService<T>
+    {
+        public abstract RestClient Client { get; }
+
+        protected T GetDeserializedObject(IRestRequest request)
+        {
+            var response = Client.Execute(request);
+
+            var deserializer = new JsonDeserializer();
+
+            return deserializer.Deserialize<T>(response);
+        }
+    }
+}
