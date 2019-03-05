0868bf94e0a71048834e486017eb37525083f786
Kiran Randhawa <Kiran.Randhawa85@gmail.com>
2019-03-05 09:35:09 +0000

    Generated Steps for the search functionality

--- CityWeather.Specs/SearchingCitiesSteps.cs
+++ CityWeather.Specs/SearchingCitiesSteps.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Castle.Components.DictionaryAdapter;
+using CityWeather.Api.Controllers;
+using CityWeather.Api.Models;
+using CityWeather.Data.Models;
+using CityWeather.Specs.TestHelpers;
+using FluentAssertions;
 using TechTalk.SpecFlow;
 
 namespace CityWeather.Specs
@@ -6,28 +15,41 @@ namespace CityWeather.Specs
     [Binding]
     public class SearchingCitiesSteps
     {
+        private List<CityApiModel> _cityEntities;
+        public CitySearchController _citySearchController;
+        private IEnumerable<CitySearchResultApiModel>_lastSearchResult;
+
+        [BeforeScenario()]
+        private void BeforeScenario()
+        {
+            _cityEntities = new List<CityApiModel>();
+           // _citySearchController = new CitySearchController();
+        }
+
         [Given(@"The city ""(.*)"" exists in the system")]
         public void GivenTheCityExistsInTheSystem(string cityName)
         {
-            
+             var newCityApiModel = ApiModelTestHelper.CreateCityModel(cityName);
+            _cityEntities.Add(newCityApiModel);
         }
         
         [When(@"The search term ""(.*)"" is used")]
-        public void WhenTheSearchTermIsUsed(string cityName)
+        public void WhenTheSearchTermIsUsed(string searchTerm)
         {
-            ScenarioContext.Current.Pending();
+            _lastSearchResult = _citySearchController.Get(searchTerm);
         }
         
         [Then(@"The search results should contain ""(.*)""")]
         public void ThenTheSearchResultsShouldContain(string cityName)
         {
-            ScenarioContext.Current.Pending();
+            _lastSearchResult.Any(x=>x.CityName == cityName).Should().BeTrue();
         }
         
         [Then(@"The number of results returned should be (.*)")]
         public void ThenTheNumberOfResultsReturnedShouldBe(int count)
         {
-            ScenarioContext.Current.Pending();
+            _lastSearchResult.Count().Should().Be(count);
+
         }
     }
 }
